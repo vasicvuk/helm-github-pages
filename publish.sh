44455181013d5ed491fa0ac37ae23cccbf21451a
vasicvuk <hasterson123@gmail.com>
2018-10-09 09:05:53 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -45,7 +45,7 @@ echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
+git clone -b "$GITHUB_PAGES_BRANCH" "https://$GITHUB_TOKEN@github.com/$GITHUB_PAGES_REPO.git" .
 
 echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
